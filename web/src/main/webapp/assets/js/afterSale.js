61b5b2c990d1c62fa16e6967721514c8c7ff60ed
chang0022 <chang.0022@163.com>
2017-05-02 14:41:34 +0800

    退货及维修原型有问题，搁置等修改

---
+++ web/src/main/webapp/assets/js/afterSale.js
@@ -0,0 +1,85 @@
+/**
+ * Created by Neo on 2017/5/2.
+ */
+$(function () {
+    "use strict";
+
+    var table = $('#repairTable').DataTable({
+        "processing": true,
+        "serverSide": true,
+        "ajax": {
+            "url": "mock/afterSale.json",
+            "data": function (d) {
+                return $.extend({}, d, extendData());
+            }
+        },
+        "ordering": false,
+        "lengthChange": false,
+        "searching": false,
+        "columns": [
+            {
+                "title": "购买用户", "data": "user"
+            },
+            {
+                "title": "手机号", "data": "phone"
+            },
+            {
+                "title": "购买型号", "data": "type"
+            },
+            {
+                "title": "商品编码", "data": "code"
+            },
+            {
+                "title": "数量", "data": "amount"
+            },
+            {
+                "title": "维修时间", "data": "time"
+            },
+            {
+                "title": "操作员", "data": "operator"
+            },
+            {
+                "title": "状态", "data": "status"
+            },
+            {
+                title: "操作",
+                className: 'table-action',
+                data: function (item) {
+                    return '<a href="javascript:;" class="js-checkUser" data-id="' + item.id + '"><i class="fa fa-check-circle-o" aria-hidden="true"></i>&nbsp;查看</a>';
+                }
+            }
+        ],
+        "displayLength": 15,
+        "drawCallback": function () {
+            clearSearchValue();
+        }
+    });
+
+    $(document).on('click', '.js-search', function () {
+        // 点击搜索方法。但如果数据为空，是否阻止
+        table.ajax.reload();
+    }).on('click', '.js-checkUser', function () {
+        // TODO
+        // 需要获取一些参数供详情跳转
+        // $('#content', parent.document).attr('src', 'userDetail.html');
+    });
+
+    // 添加额外的参数
+    function extendData() {
+        var formItem = $('.js-selectToolbar').find('.form-control');
+        if (formItem.length === 0)  return {};
+        var data = {};
+
+        formItem.each(function () {
+            var t = $(this);
+            var n = t.attr('name');
+            var v = t.val();
+            if (v) data[n] = v;
+        });
+        return data;
+    }
+
+    function clearSearchValue() {
+        //TODO
+    }
+});
