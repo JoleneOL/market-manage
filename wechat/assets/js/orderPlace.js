c1c35b1f63af61d1c62769f7da4c0a409e643619
chang0022 <chang.0022@163.com>
2017-05-08 01:07:49 +0800

    ‘我要下单’ JS 调整 待加入表单验证

--- wechat/assets/js/orderPlace.js
+++ wechat/assets/js/orderPlace.js
@@ -5,4 +5,36 @@ $(function () {
     $('#J_cityPicker').cityPicker({
         title: "请选择收货地址"
     });
+
+    $('#J_goodsAmount').keyup(function () {
+        countTotal();
+    });
+
+    $('#J_goodsType').change(function () {
+        changeAllMoney($(this));
+        countTotal();
+    });
+
+
+    function countTotal() {
+        var deposit = +$('#J_userDeposit').find('strong').text();
+        var cost = +$('#J_installationCost').find('strong').text();
+        var amout = +$('#J_goodsAmount').val();
+        var total = (deposit + cost) * amout;
+        $('#J_orderTotal').find('strong').text(total);
+    }
+
+    function changeAllMoney($ele) {
+        var $type = $ele.find('option:checked');
+        var deposit = $type.attr('data-deposit');
+        var isNeed = $type.attr('data-need-install');
+        if (isNeed) {
+            $('.js-install').show();
+            $('#J_installationCost').find('strong').text(isNeed);
+        } else {
+            $('.js-install').hide();
+            $('#J_installationCost').find('strong').text(0);
+        }
+        $('#J_userDeposit').find('strong').text(deposit);
+    }
 });
