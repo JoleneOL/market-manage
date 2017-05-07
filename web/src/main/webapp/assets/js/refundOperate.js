3676a3cd25f4d65a25fe2a25e8e564e9b71b04e6
chang0022 <chang.0022@163.com>
2017-05-07 15:43:17 +0800

    代理商现有原型完结

--- web/src/main/webapp/assets/js/refundOperate.js
+++ web/src/main/webapp/assets/js/refundOperate.js
@@ -1,88 +1,25 @@
 /**
- * Created by Neo on 2017/5/6.
+ * Created by Chang on 2017/5/7.
  */
 $(function () {
-    "use strict";
-
-    var table = $('#refundTable').DataTable({
-        "processing": true,
-        "serverSide": true,
-        "ajax": {
-            "url": "mock/refundOperate.json",
-            "data": function (d) {
-                return $.extend({}, d, extendData());
-            }
-        },
-        "ordering": false,
-        "lengthChange": false,
-        "searching": false,
-        "columns": [
-            {
-                "title": "购买用户", "data": "user", "name": "user"
-            },
-            {
-                "title": "手机号", "data": "phone", "name": "phone"
-            },
-            {
-                "title": "购买型号", "data": "type", "name": "type"
-            },
-            {
-                "title": "商品编码", "data": "code", "name": "code"
-            },
-            {
-                "title": "数量", "data": "amount", "name": "amount"
-            },
-            {
-                "title": "发起时间", "data": "time", "name": "time"
-            },
-            {
-                "title": "操作员", "data": "operator", "name": "operator"
-            },
-            {
-                "title": "状态", "data": "status", "name": "status"
-            },
-            {
-                title: "操作",
-                className: 'table-operate',
-                data: function (item) {
-                    var a = '<a href="javascript:;" class="js-checkUser" data-id="' + item.id + '"><i class="fa fa-check-circle-o" aria-hidden="true"></i>&nbsp;退款详情</a>';
-                    var b = '<a href="javascript:;" class="js-checkLogistics" data-id="' + item.id + '"><i class="fa fa-check-circle-o" aria-hidden="true"></i>&nbsp;查看物流</a>';
-                    if(item.statusCode === 2)  return a + b;
-                    return a;
-                }
-            }
-        ],
-        "displayLength": 15,
-        "drawCallback": function () {
-            clearSearchValue();
-        }
+    $('#J_date').datetimepicker({
+        todayBtn: "linked",
+        language: "zh-CN",
+        autoclose: true,
+        format: "yyyy-m-d",
+        clearBtn: true,
+        todayHighlight: true,
+        minView: 2,
+        startDate: new Date()
     });
 
-    $(document).on('click', '.js-search', function () {
-        // 点击搜索方法。但如果数据为空，是否阻止
-        table.ajax.reload();
-    }).on('click', '.js-checkUser', function () {
-        // TODO
-        // 需要获取一些参数供详情跳转
-        $('#content', parent.document).attr('src', 'repairDetail.html');
+    $('#J_time').datetimepicker({
+        language: "zh-CN",
+        format: "h:i",
+        autoclose: true,
+        clearBtn: true,
+        startView: 1,
+        minView: 0,
+        maxView: 1
     });
-
-    // 添加额外的参数
-    function extendData() {
-        var formItem = $('.js-selectToolbar').find('.form-control');
-        if (formItem.length === 0)  return {};
-        var data = {};
-
-        formItem.each(function () {
-            var t = $(this);
-            var n = t.attr('name');
-            var v = t.val();
-            if (v) data[n] = v;
-        });
-        return data;
-    }
-
-    function clearSearchValue() {
-        //TODO
-    }
 });
---
+++ web/src/main/webapp/assets/js/refundPage.js
@@ -0,0 +1,91 @@
+/**
+ * Created by Neo on 2017/5/6.
+ */
+$(function () {
+    "use strict";
+
+    var table = $('#refundTable').DataTable({
+        "processing": true,
+        "serverSide": true,
+        "ajax": {
+            "url": "mock/refundOperate.json",
+            "data": function (d) {
+                return $.extend({}, d, extendData());
+            }
+        },
+        "ordering": false,
+        "lengthChange": false,
+        "searching": false,
+        "columns": [
+            {
+                "title": "购买用户", "data": "user", "name": "user"
+            },
+            {
+                "title": "手机号", "data": "phone", "name": "phone"
+            },
+            {
+                "title": "购买型号", "data": "type", "name": "type"
+            },
+            {
+                "title": "商品编码", "data": "code", "name": "code"
+            },
+            {
+                "title": "数量", "data": "amount", "name": "amount"
+            },
+            {
+                "title": "发起时间", "data": "time", "name": "time"
+            },
+            {
+                "title": "操作员", "data": "operator", "name": "operator"
+            },
+            {
+                "title": "状态", "data": "status", "name": "status"
+            },
+            {
+                title: "操作",
+                className: 'table-operate',
+                data: function (item) {
+                    var a = '<a href="javascript:;" class="js-checkInfo" data-id="' + item.id + '"><i class="fa fa-check-circle-o" aria-hidden="true"></i>&nbsp;退款详情</a>';
+                    var b = '<a href="javascript:;" class="js-checkLogistics" data-id="' + item.id + '"><i class="fa fa-check-circle-o" aria-hidden="true"></i>&nbsp;查看物流</a>';
+                    if (item.statusCode === 2)  return a + b;
+                    return a;
+                }
+            }
+        ],
+        "displayLength": 15,
+        "drawCallback": function () {
+            clearSearchValue();
+        }
+    });
+
+    $(document).on('click', '.js-search', function () {
+        // 点击搜索方法。但如果数据为空，是否阻止
+        table.ajax.reload();
+    }).on('click', '.js-checkInfo', function () {
+        // TODO
+        // 需要获取一些参数供详情跳转
+        var url = Math.random() > 0.5 ? 'refundWait.html': 'refundSuccess.html';
+        $('#content', parent.document).attr('src', url);
+    }).on('click', '.js-checkLogistics', function () {
+        $('#content', parent.document).attr('src', 'refundLogisticsDetail.html');
+    });
+
+    // 添加额外的参数
+    function extendData() {
+        var formItem = $('.js-selectToolbar').find('.form-control');
+        if (formItem.length === 0)  return {};
+        var data = {};
+
+        formItem.each(function () {
+            var t = $(this);
+            var n = t.attr('name');
+            var v = t.val();
+            if (v) data[n] = v;
+        });
+        return data;
+    }
+
+    function clearSearchValue() {
+        //TODO
+    }
+});
