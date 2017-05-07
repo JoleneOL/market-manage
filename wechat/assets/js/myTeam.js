0135000c6982510d6dcd73cdc1230b096e658104
chang0022 <chang.0022@163.com>
2017-05-08 00:49:22 +0800

    添加‘我的团队’ ‘我要下单’

---
+++ wechat/assets/js/myTeam.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Chang on 2017/5/7.
+ */
+$(function () {
+    $('#J_upgradeRules').click(function () {
+        $.toast("满30个人即可升级", "text");
+    });
+
+});
---
+++ wechat/assets/js/orderPlace.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Chang on 2017/5/7.
+ */
+$(function () {
+    $('#J_cityPicker').cityPicker({
+        title: "请选择收货地址"
+    });
+});
