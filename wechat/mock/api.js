6fe87eb96cf35d267c7d7e35d5e98dcda11129d1
chang0022 <chang.0022@163.com>
2017-05-11 00:01:22 +0800

    myTeam 调整

--- wechat/mock/api.js
+++ wechat/mock/api.js
@@ -10,32 +10,32 @@ Mock.setup({
  * resultCode 多个200，减少错误概率 ~囧~
  */
 Mock.mock(/\/api\/teamList/, "get", {
-    "resultCode": '@pick([200, 200, 200, 500])',
+    "resultCode": 200,
     "resultMsg": "ok",
     "data|10": [
         {
             name: "@cname",
-            rank: '@pick(["会员", "代理商", "经销商", "普通用户"])',
-            joinTime: '@datetime("yyyy-MM-dd")',
-            phone: '@integer(10000000000, 18888888888)'
+            rank: '@pick(["总代理", "分代理", "经销商", "爱心天使"])',
+            joinTime: '@now("yyyy-MM-dd")',
+            phone: /^1(3|4|5|7|8)\d{9}$/
         }
     ]
 });
 
 Mock.mock(/\/api\/orderList/, "get", {
-    "resultCode": '@pick([200, 200, 200, 500])',
+    "resultCode": 200,
     "resultMsg": "ok",
     "data|10": [
         {
             orderId: '@id',
-            orderTime: '@datetime("yyyy-MM-dd")',
+            orderTime: '@now("yyyy-MM-dd")',
             orderStatus: '@pick(["成功", "失败", "取消"])',
             name: "@cname",
             goodsInfo: '饮水机 u56立式',
             orderAmount: '@integer(3000, 9999999)',
             package: '3年收费 730天',
             quantity: '@integer(1, 100)',
-            phone: '@integer(10000000000, 18888888888)'
+            phone: /^1(3|4|5|7|8)\d{9}$/
         }
     ]
 });
