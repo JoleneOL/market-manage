fe5ebd5796978bba4ad56c425ccc6f781022d67d
Cai Jiang <luffy.ja@gmail.com>
2017-05-16 03:29:19 +0800

    单独设置推荐人模块

--- dealer/src/main/resources/dealer-resource/js/addAgent.js
+++ dealer/src/main/resources/dealer-resource/js/addAgent.js
@@ -61,76 +61,7 @@ $(function () {
         }
 
     });
-    $("#referrerPhone").select2({
-        theme: "bootstrap",
-        width: null,
-        containerCssClass: ':all:',
-        placeholder: "选择一个推荐人",
-        allowClear: true,
-        language: "zh-CN",
-        ajax: {
-            url: body.attr('data-search-login-url'),
-            dataType: 'json',
-            delay: 250,
-            data: function (params) {
-                return {
-                    search: params.term, // search term
-                    page: params.page
-                };
-            },
-            processResults: function (data, params) {
-                // parse the results into the format expected by Select2
-                // since we are using custom formatting functions we do not need to
-                // alter the remote JSON data, except to indicate that infinite
-                // scrolling can be used
-                params.page = params.page || 1;
-
-                return {
-                    results: data.items,
-                    pagination: {
-                        more: (params.page * 30) < data.total_count
-                    }
-                };
-            },
-            cache: true
-        },
-        escapeMarkup: function (markup) {
-            return markup;
-        }, // let our custom formatter work
-        minimumInputLength: 2,
-        templateResult: function (x) {
-            if (x.id == '')
-                return x.text;
-            // 渲染html宽体
-            // console.log('templateResult', x);
-            var strName;
-            if (x.name)
-                strName = '<p>姓名:' + x.name + '</p>';
-            else
-                strName = '';
-
-            var strMobile;
-            if (x.mobile)
-                strMobile = '<p>电话:' + x.mobile + '</p>';
-            else
-                strMobile = '';
-
-            return strName + strMobile;
-        },
-        templateSelection: function (x) {
-            if (x.id == '')
-                return x.text;
-            // 渲染当前选择
-            if (x.name && x.mobile)
-                return x.name + '(' + x.mobile + ')';
-            if (!x.name && !x.mobile)
-                return '';
-            if (x.name)
-                return x.name;
-            return x.mobile;
-        }
-
-    });
+    $("#referrerPhone").makeRecommendSelect();
     // var x = $('#referrerPhone').searchableSelect({
     //     afterSelectItem:function (x) {
     //         console.log('select ',x);
