4c8399f48158ecfff891ba854cd973a92bf2f815
Cai Jiang <luffy.ja@gmail.com>
2017-05-16 03:47:16 +0800

    添加控制器，准备测试

--- core-service/src/main/java/cn/lmjia/market/core/controller/main/order/AbstractMainOrderController.java
+++ core-service/src/main/java/cn/lmjia/market/core/controller/main/order/AbstractMainOrderController.java
@@ -1,7 +1,12 @@
 package cn.lmjia.market.core.controller.main.order;
 
 import cn.lmjia.market.core.entity.Login;
+import cn.lmjia.market.core.entity.MainOrder;
+import cn.lmjia.market.core.entity.support.Address;
 import cn.lmjia.market.core.repository.MainGoodRepository;
+import cn.lmjia.market.core.service.LoginService;
+import cn.lmjia.market.core.service.MainOrderService;
+import me.jiangcai.wx.model.Gender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 
@@ -11,6 +16,10 @@ import org.springframework.ui.Model;
 public abstract class AbstractMainOrderController {
     @Autowired
     private MainGoodRepository mainGoodRepository;
+    @Autowired
+    private MainOrderService mainOrderService;
+    @Autowired
+    private LoginService loginService;
 
     /**
      * 展示下单页面
@@ -20,5 +29,11 @@ public abstract class AbstractMainOrderController {
      */
     protected void orderIndex(Login login, Model model) {
         model.addAttribute("goodList", mainGoodRepository.findByEnableTrue());
+    }
+
+    protected MainOrder newOrder(Login login, Model model, long recommendId, String name, int age, Gender gender
+            , Address address, String mobile, long goodId, int amount, String mortgageIdentifier) {
+        return mainOrderService.newOrder(login, loginService.get(recommendId), name, mobile, age, gender, address
+                , mainGoodRepository.getOne(goodId), amount, mortgageIdentifier);
     }
 }
--- wechat-service/src/main/java/cn/lmjia/market/wechat/controller/order/WechatMainOrderController.java
+++ wechat-service/src/main/java/cn/lmjia/market/wechat/controller/order/WechatMainOrderController.java
@@ -2,10 +2,13 @@ package cn.lmjia.market.wechat.controller.order;
 
 import cn.lmjia.market.core.controller.main.order.AbstractMainOrderController;
 import cn.lmjia.market.core.entity.Login;
+import cn.lmjia.market.core.entity.support.Address;
+import me.jiangcai.wx.model.Gender;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * @author CJ
@@ -20,6 +23,16 @@ public class WechatMainOrderController extends AbstractMainOrderController {
     public String index(@AuthenticationPrincipal Login login, Model model) {
         orderIndex(login, model);
         return "wechat@orderPlace.html";
+    }
+
+    // name=%E5%A7%93%E5%90%8D&age=99&gender=2
+    // &address=%E6%B5%99%E6%B1%9F%E7%9C%81+%E6%9D%AD%E5%B7%9E%E5%B8%82+%E6%BB%A8%E6%B1%9F%E5%8C%BA
+    // &fullAddress=%E6%B1%9F%E7%95%94%E6%99%95%E5%95%A6&mobile=18606509616&goodId=2&leasedType=hzts02&amount=0&activityCode=xzs&recommend=2
+    @PostMapping("/wechatOrder")
+    public String newOrder(String name, int age, Gender gender, Address address, String mobile, long goodId, int amount
+            , String activityCode, long recommend, @AuthenticationPrincipal Login login, Model model) {
+        newOrder(login, model, recommend, name, age, gender, address, mobile, goodId, amount, activityCode);
+        return null;
     }
 
 }
