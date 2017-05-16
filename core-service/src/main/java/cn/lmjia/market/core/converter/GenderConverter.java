2126223a271e501de00b1d8ccdf5a5309fcf8ea4
Cai Jiang <luffy.ja@gmail.com>
2017-05-16 16:36:53 +0800

    准备支付！

---
+++ core-service/src/main/java/cn/lmjia/market/core/converter/GenderConverter.java
@@ -0,0 +1,10 @@
+package cn.lmjia.market.core.converter;
+
+import me.jiangcai.lib.spring.converter.EnumFormatter;
+import me.jiangcai.wx.model.Gender;
+
+/**
+ * @author CJ
+ */
+public class GenderConverter extends EnumFormatter<Gender> {
+}
