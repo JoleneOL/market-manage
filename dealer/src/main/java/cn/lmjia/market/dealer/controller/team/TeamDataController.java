965fc2109685bf12388bc8b86db2991f46f9ba21
Cai Jiang <luffy.ja@gmail.com>
2017-05-31 00:09:20 +0800

    至少重构是完成了

--- dealer/src/main/java/cn/lmjia/market/dealer/controller/team/TeamDataController.java
+++ dealer/src/main/java/cn/lmjia/market/dealer/controller/team/TeamDataController.java
@@ -11,6 +11,7 @@ import cn.lmjia.market.core.service.ReadService;
 import cn.lmjia.market.core.service.SystemService;
 import cn.lmjia.market.core.util.ApiDramatizer;
 import cn.lmjia.market.dealer.service.AgentService;
+import me.jiangcai.lib.spring.data.AndSpecification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.convert.ConversionService;
@@ -21,10 +22,6 @@ import org.springframework.util.NumberUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.persistence.criteria.AbstractQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 import java.util.Arrays;
 import java.util.List;
 
@@ -60,79 +57,114 @@ public class TeamDataController {
         return systemService.systemLevel() - 1 + x - 3;
     }
 
-
     @RowCustom(dramatizer = ApiDramatizer.class, distinct = false)
-    @GetMapping("/api/teamList")
+    @GetMapping("/api/teamList2")
     public RowDefinition<LoginRelation> teamList(@AuthenticationPrincipal Login login, String rank) {
         if (conversionService == null)
             conversionService = applicationContext.getBean(ConversionService.class);
         Integer level = fromRank(rank);
-        return new RowDefinition<LoginRelation>() {
-            @Override
-            public Class<LoginRelation> entityClass() {
-                return LoginRelation.class;
-            }
-
-//            @Override
-//            public Expression<?> count(CriteriaQuery<Long> countQuery, CriteriaBuilder criteriaBuilder, Root<LoginRelation> root) {
-//                Subquery<Long> sub = countQuery.subquery(Long.class);
-//                Root<LoginRelation> relationRoot = sub.from(LoginRelation.class);
-//                sub = sub.where(newSpecification(relationRoot, sub, criteriaBuilder));
-//                sub = sub.select(relationRoot.get("to"));
-//                sub = sub.distinct(true);
-//
-//                return criteriaBuilder.count(sub);
-//            }
+        // 推荐的人哦
+        return new LoginRelationRows(level) {
 
             @Override
-            public List<FieldDefinition<LoginRelation>> fields() {
-                return Arrays.asList(
-                        FieldBuilder.asName(LoginRelation.class, "name")
-                                .addBiSelect((loginRelationRoot, criteriaBuilder)
-                                        -> ReadService.nameForLogin(loginRelationRoot.join("to"), criteriaBuilder))
-                                .build()
-                        , FieldBuilder.asName(LoginRelation.class, "rank")
-                                .addBiSelect((loginRelationRoot, criteriaBuilder)
-                                        -> criteriaBuilder.min(loginRelationRoot.get("level")))
-                                .addFormat((o, mediaType) -> agentService.getLoginTitle((Integer) o))
-                                .build()
-                        , FieldBuilder.asName(LoginRelation.class, "joinTime")
-                                .addSelect(loginRelationRoot -> loginRelationRoot.get("to").get("createdTime"))
-                                .addFormat((o, mediaType) -> conversionService.convert(o, String.class))
-                                .build()
-                        , FieldBuilder.asName(LoginRelation.class, "phone")
-                                .addBiSelect((loginRelationRoot, criteriaBuilder)
-                                        -> ReadService.mobileForLogin(loginRelationRoot.join("to"), criteriaBuilder))
-                                .build()
-                );
-            }
-
-            private Predicate newSpecification(Root<LoginRelation> root, AbstractQuery<?> query, CriteriaBuilder cb) {
-//                if (!(query instanceof Subquery) && query.getResultType() == Long.class) {
-//                    return cb.isTrue(cb.literal(true));
-//                }
-                query.groupBy(root.get("to"));
-                Predicate predicate = cb.equal(root.get("from"), login);
-
-//                    if (level == null)
-//                        query.having(predicate);
-//                    else
-//                        query.having(predicate
-//                                , cb.equal(root.get("level"), level));
-//
-//                    return cb.isTrue(cb.literal(true));
-                if (level == null)
-                    return predicate;
-                return cb.and(predicate
-                        , cb.equal(root.get("level"), level));
-            }
-
-            @Override
-            public Specification<LoginRelation> specification() {
-                return this::newSpecification;
+            Specification<LoginRelation> newSpecification() {
+                return null;
             }
         };
     }
 
 
+    @RowCustom(dramatizer = ApiDramatizer.class, distinct = false)
+    @GetMapping("/api/teamList")
+    public RowDefinition<LoginRelation> teamList2(@AuthenticationPrincipal Login login, String rank) {
+        if (conversionService == null)
+            conversionService = applicationContext.getBean(ConversionService.class);
+        Integer level = fromRank(rank);
+        return new LoginRelationRows(level) {
+
+            @Override
+            Specification<LoginRelation> newSpecification() {
+                return (root, query, cb) -> {
+                    query.groupBy(root.get("to"));
+                    return cb.equal(root.get("from"), login);
+                };
+            }
+        };
+    }
+
+
+    private abstract class LoginRelationRows implements RowDefinition<LoginRelation> {
+
+        private final Integer level;
+
+        protected LoginRelationRows(Integer level) {
+            this.level = level;
+        }
+
+        @Override
+        public Class<LoginRelation> entityClass() {
+            return LoginRelation.class;
+        }
+
+        @Override
+        public List<FieldDefinition<LoginRelation>> fields() {
+            return Arrays.asList(
+                    FieldBuilder.asName(LoginRelation.class, "name")
+                            .addBiSelect((loginRelationRoot, criteriaBuilder)
+                                    -> ReadService.nameForLogin(loginRelationRoot.join("to"), criteriaBuilder))
+                            .build()
+                    , FieldBuilder.asName(LoginRelation.class, "rank")
+                            .addBiSelect((loginRelationRoot, criteriaBuilder)
+                                    -> criteriaBuilder.min(loginRelationRoot.get("level")))
+                            .addFormat((o, mediaType) -> agentService.getLoginTitle((Integer) o))
+                            .build()
+                    , FieldBuilder.asName(LoginRelation.class, "joinTime")
+                            .addSelect(loginRelationRoot -> loginRelationRoot.get("to").get("createdTime"))
+                            .addFormat((o, mediaType) -> conversionService.convert(o, String.class))
+                            .build()
+                    , FieldBuilder.asName(LoginRelation.class, "phone")
+                            .addBiSelect((loginRelationRoot, criteriaBuilder)
+                                    -> ReadService.mobileForLogin(loginRelationRoot.join("to"), criteriaBuilder))
+                            .build()
+            );
+        }
+
+        private Specification<LoginRelation> fixedSpecification() {
+            if (level == null)
+                return null;
+            return (root, query, cb) -> cb.equal(root.get("level"), level);
+        }
+
+        @Override
+        public final Specification<LoginRelation> specification() {
+            return new AndSpecification<>(fixedSpecification(), newSpecification());
+        }
+
+        abstract Specification<LoginRelation> newSpecification();
+
+        //        private Predicate newSpecification(Root<LoginRelation> root, AbstractQuery<?> query, CriteriaBuilder cb) {
+////                if (!(query instanceof Subquery) && query.getResultType() == Long.class) {
+////                    return cb.isTrue(cb.literal(true));
+////                }
+//            query.groupBy(root.get("to"));
+//            Predicate predicate = cb.equal(root.get("from"), login);
+//
+////                    if (level == null)
+////                        query.having(predicate);
+////                    else
+////                        query.having(predicate
+////                                , cb.equal(root.get("level"), level));
+////
+////                    return cb.isTrue(cb.literal(true));
+//            if (level == null)
+//                return predicate;
+//            return cb.and(predicate
+//                    , cb.equal(root.get("level"), level));
+//        }
+//
+//        @Override
+//        public Specification<LoginRelation> specification() {
+//            return this::newSpecification;
+//        }
+    }
 }
