3b95bcaae8fe0cb255723f0a578367fd93bea13e
Cai Jiang <luffy.ja@gmail.com>
2017-06-05 18:39:59 +0800

    食品优化宝,9800

---
+++ core-service/src/test/java/cn/lmjia/market/core/service/QRCodeServiceTest.java
@@ -0,0 +1,26 @@
+package cn.lmjia.market.core.service;
+
+import cn.lmjia.market.core.CoreServiceTest;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+
+/**
+ * @author CJ
+ */
+@Ignore
+public class QRCodeServiceTest extends CoreServiceTest {
+
+    @Autowired
+    private QRCodeService qrCodeService;
+
+    @Test
+    public void generateQRCode() throws Exception {
+        BufferedImage image = qrCodeService.generateQRCode("weixin://wxpay/bizpayurl?pr=THkIR3X");
+        JOptionPane.showMessageDialog(null, new ImageIcon(image));
+    }
+
+}
