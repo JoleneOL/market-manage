4d8def3a9701601dfe6d67b7984315a2fbc24c8f
chang0022 <chang.0022@163.com>
2017-06-19 17:58:19 +0800

    把微信端的尾巴解决。希望需求不再更改~

--- wechat-service/src/main/resources/wechat-resource/assets/js/equipment.js
+++ wechat-service/src/main/resources/wechat-resource/assets/js/equipment.js
@@ -42,6 +42,12 @@ $(function () {
     });
 
 
+    // 粗略的手机号正则
+    $.validator.addMethod("isPhone", function (value, element) {
+        var mobile = /^1([34578])\d{9}$/;
+        return this.optional(element) || (mobile.test(value));
+    }, "请正确填写的手机号");
+
     $('#J_form').validate({
         rules: {
             logisticsCompany: "required",
@@ -59,6 +65,73 @@ $(function () {
         },
         unhighlight: function (element, errorClass, validClass) {
             $(element).closest('.weui-cell').removeClass("weui-cell_warn");
+        }
+    });
+
+
+    $('#J_maintainForm').validate({
+        rules: {
+            appointment: "required"
+        },
+        messages: {
+            logisticsCompany: "请选择预约时间"
+        },
+        errorPlacement: function (error, element) {
+            $.toptip(error);
+        },
+        highlight: function (element, errorClass, validClass) {
+            $(element).closest('.weui-cell').addClass("weui-cell_warn")
+        },
+        unhighlight: function (element, errorClass, validClass) {
+            $(element).closest('.weui-cell').removeClass("weui-cell_warn");
+        }
+    });
+
+
+    $('select[name="maintainType"]').change(function () {
+        if( +$(this).val() === 1) {
+            $('#J_require').show();
+            $('#J_moving').hide();
+            $('input[name="appointment"]').rules('add', {
+                required: true,
+                messages : {
+                    required : "请选择预约时间"
+                }
+            });
+            $('input[name="moveAddress"]').rules('remove');
+            $('input[name="moveName"]').rules('remove');
+            $('input[name="movePhone"]').rules('remove');
+        } else {
+            $('#J_require').hide();
+            $('#J_moving').show();
+            $('input[name="appointment"]').rules('remove');
+            $('input[name="moveAddress"]').rules('add', {
+                required: true,
+                messages : {
+                    required : "请选择移机地址"
+                }
+            });
+            $('input[name="moveName"]').rules('add', {
+                required: true,
+                messages : {
+                    required : "请填写联系人"
+                }
+            });
+            $('input[name="movePhone"]').rules('add', {
+                required: true,
+                isPhone: true,
+                messages : {
+                    required : "请填写联系电话"
+                }
+            });
+
+        }
+    });
+
+    $('#J_moveAddress').cityPicker({
+        title: "请选择收货地址",
+        onChange: function () {
+            $('#J_cityPicker').closest('.weui-cell').removeClass('weui-cell_warn');
         }
     });
 });
