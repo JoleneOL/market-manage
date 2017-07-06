1d428a76629d2f11ee08c5a13dbb716abcc4b6dc
Cai Jiang <luffy.ja@gmail.com>
2017-07-06 14:45:59 +0800

    测试大致的流程

---
+++ wechat-service/src/test/java/cn/lmjia/market/wechat/controller/WechatShareControllerTest.java
@@ -0,0 +1,21 @@
+package cn.lmjia.market.wechat.controller;
+
+import cn.lmjia.market.wechat.WechatTestBase;
+import org.junit.Test;
+
+/**
+ * @author CJ
+ */
+public class WechatShareControllerTest extends WechatTestBase {
+    @Test
+    public void share() throws Exception {
+        // 正常用户进入
+        // 打开分享页面
+        // 模拟新用户分享
+        // 模拟新用户进入
+        // 应该进入注册
+        // 完成注册
+        // 应该进入下单
+    }
+
+}
