423464c6b8e0506780ffce1ccf20be7223763a8b
Cai Jiang <luffy.ja@gmail.com>
2017-07-18 22:07:32 +0800

    可用于测试了。

--- logistics-haier/src/main/java/me/jiangcai/logstics/haier/service/HaierSupplierImpl.java
+++ logistics-haier/src/main/java/me/jiangcai/logstics/haier/service/HaierSupplierImpl.java
@@ -1,17 +1,33 @@
 package me.jiangcai.logstics.haier.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import me.jiangcai.logistics.Destination;
 import me.jiangcai.logistics.Source;
 import me.jiangcai.logistics.Storage;
 import me.jiangcai.logistics.Thing;
 import me.jiangcai.logistics.entity.Distribution;
+import me.jiangcai.logistics.exception.SupplierException;
 import me.jiangcai.logistics.option.LogisticsOptions;
 import me.jiangcai.logstics.haier.HaierSupplier;
+import me.jiangcai.logstics.haier.http.ResponseHandler;
+import org.apache.http.client.entity.EntityBuilder;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
+import java.util.Base64;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -25,13 +41,15 @@ import java.util.stream.Collectors;
 @Component
 public class HaierSupplierImpl implements HaierSupplier {
 
+    private final static ObjectMapper objectMapper = new ObjectMapper();
     private final String gateway;
+    private final String keyValue;
 
     @Autowired
     public HaierSupplierImpl(Environment environment) {
         this.gateway = environment.getProperty("haier.gateway.URL", "http://58.56.128.84:9001/EAI/service/VOM/CommonGetWayToVOM/CommonGetWayToVOM");
+        this.keyValue = environment.getProperty("haier.keyValue", "Haier,123");
     }
-
 
     @Override
     public Distribution makeDistributionOrder(Source source, Collection<Thing> things, Destination destination, int options) {
@@ -61,11 +79,59 @@ public class HaierSupplierImpl implements HaierSupplier {
         List<Map<String, Object>> items = things.stream()
                 .map(this::toItemData)
                 .collect(Collectors.toList());
+        for (int i = 0; i < items.size(); i++) {
+            items.get(i).put("itemno", i + 1);
+        }
 
         parameters.put("items", items);
+        //参数准备完成
+        try {
 
+            String content = objectMapper.writeValueAsString(parameters);
 
-        return null;
+            try (CloseableHttpClient client = newClient()) {
+                HttpPost post = new HttpPost(gateway);
+                post.setEntity(
+                        EntityBuilder.create()
+                                .setContentType(ContentType.APPLICATION_FORM_URLENCODED.withCharset(Charset.forName("UTF-8")))
+                                .setParameters(
+                                        new BasicNameValuePair("notifyid", UUID.randomUUID().toString().replaceAll("-", ""))
+                                        , new BasicNameValuePair("notifytime", LocalDateTime.now().format(formatter))
+                                        , new BasicNameValuePair("butype", "rrs_order")
+                                        , new BasicNameValuePair("source", "JSH")
+                                        , new BasicNameValuePair("type", "Json")
+                                        , new BasicNameValuePair("sign", sign(content))
+                                        , new BasicNameValuePair("content", content)
+                                )
+                                .build()
+                );
+
+                client.execute(post, new ResponseHandler());
+
+                Distribution distribution = new Distribution();
+                distribution.setId(id);
+                return distribution;
+            }
+        } catch (IOException e) {
+            throw new SupplierException(e);
+        }
+
+    }
+
+    @SneakyThrows({NoSuchAlgorithmException.class, UnsupportedEncodingException.class})
+    private String sign(String content) {
+        /*
+        "签名 base64(MD5(content+keyValue))
+ketValue：Haier,123
+content：如下"
+         */
+        byte[] digest = MessageDigest.getInstance("MD5").digest((content + keyValue).getBytes("UTF-8"));
+        return Base64.getEncoder().encodeToString(digest);
+    }
+
+    private CloseableHttpClient newClient() {
+        return HttpClientBuilder.create()
+                .build();
     }
 
     private Map<String, Object> toItemData(Thing thing) {
--- logistics/src/main/java/me/jiangcai/logistics/entity/Distribution.java
+++ logistics/src/main/java/me/jiangcai/logistics/entity/Distribution.java
@@ -1,9 +1,22 @@
 package me.jiangcai.logistics.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+
 /**
  * 可追踪配送
  *
  * @author CJ
  */
-public abstract class Distribution {
+@Setter
+@Getter
+public class Distribution {
+
+    @Id
+    @Column(length = 64)
+    private String id;
+
 }
