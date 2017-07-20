5ba5b698559381046a5f466a2b6fb31d8cad4615
Cai Jiang <luffy.ja@gmail.com>
2017-07-20 10:32:35 +0800

    解决测试问题；以及修复 #82

--- wechat-service/src/test/java/cn/lmjia/market/wechat/controller/order/WechatMainOrderControllerTest.java
+++ wechat-service/src/test/java/cn/lmjia/market/wechat/controller/order/WechatMainOrderControllerTest.java
@@ -1,5 +1,6 @@
 package cn.lmjia.market.wechat.controller.order;
 
+import cn.lmjia.market.core.config.other.SecurityConfig;
 import cn.lmjia.market.core.entity.Login;
 import cn.lmjia.market.core.service.SystemService;
 import cn.lmjia.market.wechat.WechatTestBase;
@@ -7,9 +8,8 @@ import cn.lmjia.market.wechat.page.PaySuccessPage;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-
-import java.util.concurrent.Callable;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -17,9 +17,46 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 /**
  * @author CJ
  */
+@ContextConfiguration(classes = SecurityConfig.class)
 public class WechatMainOrderControllerTest extends WechatTestBase {
 
     private static final Log log = LogFactory.getLog(WechatMainOrderControllerTest.class);
+
+    private void doOrder() throws Exception {
+        mockMvc.perform(wechatGet(SystemService.wechatOrderURi))
+//                    .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("wechat@orderPlace.html"));
+        // 这波则是开干了
+        //
+        while (true) {
+            try {
+
+                MockHttpServletRequestBuilder requestBuilder =
+                        orderRequestBuilder(wechatPost("/wechatOrder"), randomOrderRequest());
+
+                String result = mockMvc.perform(
+                        requestBuilder
+                )
+                        .andExpect(status().is3xxRedirection())
+                        .andReturn().getResponse().getHeader("Location");
+
+                // 使用 driver 打开!
+                driver.get("http://localhost" + result);
+//                    mockMvc.perform(wechatGet("/paySuccess?mainOrderId=1"))
+//                            .andDo(print());
+                PaySuccessPage page = PaySuccessPage.waitingForSuccess(this, driver, 3);
+                // 然后模拟订单完成支付
+                return;
+            } catch (AssertionError error) {
+                if (!error.getMessage().contains("200"))
+                    throw error;
+                log.info("那就再试一次呗");
+                Thread.sleep(3000);
+            }
+
+        }
+    }
 
     @Test
     public void makeOrder() throws Exception {
@@ -27,46 +64,15 @@ public class WechatMainOrderControllerTest extends WechatTestBase {
         Login login1 = randomLogin(false);
         // 特别的设计，让这个帐号绑定到我个人微信openId 确保可以收到消息
         bindDeveloperWechat(login1);
-        final Callable<Object> callable = () -> {
-            mockMvc.perform(wechatGet(SystemService.wechatOrderURi))
-//                    .andDo(print())
-                    .andExpect(status().isOk())
-                    .andExpect(view().name("wechat@orderPlace.html"));
-            // 这波则是开干了
-            //
-            while (true) {
-                try {
+        updateAllRunWith(login1);
 
-                    MockHttpServletRequestBuilder requestBuilder =
-                            orderRequestBuilder(wechatPost("/wechatOrder"), randomOrderRequest());
-
-                    String result = mockMvc.perform(
-                            requestBuilder
-                    )
-                            .andExpect(status().is3xxRedirection())
-                            .andReturn().getResponse().getHeader("Location");
-
-                    // 使用 driver 打开!
-                    driver.get("http://localhost" + result);
-//                    mockMvc.perform(wechatGet("/paySuccess?mainOrderId=1"))
-//                            .andDo(print());
-                    PaySuccessPage page = PaySuccessPage.waitingForSuccess(this, driver, 3);
-                    // 然后模拟订单完成支付
-                    return null;
-                } catch (AssertionError error) {
-                    if (!error.getMessage().contains("200"))
-                        throw error;
-                    log.info("那就再试一次呗");
-                    Thread.sleep(3000);
-                }
-
-            }
-        };
-        runWith(login1, callable);
+        doOrder();
         // 客户也可以下单
         final String customerMobile = randomMobile();
         newRandomOrderFor(login1, login1, customerMobile);
-        runWith(loginService.byLoginName(customerMobile), callable);
+
+        updateAllRunWith(loginService.byLoginName(customerMobile));
+        doOrder();
     }
 
 }
