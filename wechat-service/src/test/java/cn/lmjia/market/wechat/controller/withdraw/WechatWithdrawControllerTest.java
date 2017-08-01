35f04ba60fd15595c228b77c1f555dbfe0f9d243
Cai Jiang <luffy.ja@gmail.com>
2017-08-01 10:51:34 +0800

    关于测试

--- wechat-service/src/test/java/cn/lmjia/market/wechat/controller/withdraw/WechatWithdrawControllerTest.java
+++ wechat-service/src/test/java/cn/lmjia/market/wechat/controller/withdraw/WechatWithdrawControllerTest.java
@@ -1,24 +1,49 @@
 package cn.lmjia.market.wechat.controller.withdraw;
 
-
 import cn.lmjia.market.core.config.other.SecurityConfig;
 import cn.lmjia.market.core.entity.Login;
-import cn.lmjia.market.core.service.SystemService;
 import cn.lmjia.market.wechat.WechatTestBase;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @ContextConfiguration(classes = SecurityConfig.class)
 public class WechatWithdrawControllerTest extends WechatTestBase {
 
     private static final Log log = LogFactory.getLog(WechatWithdrawControllerTest.class);
+
+    @Test
+    public void go() {
+        // 测试就是校验我们的工作成功
+        // 就提现这个功能而言 我们要做的测试很简单
+        // 1，新用户
+        // 尝试提现 会看到可以可提现金额为0
+        // 强行输入提现 比如 1元 会看到错误信息
+        //
+        // 2，新用户
+        // 成功下了一笔订单 获得佣金X
+        // 尝试提现 会看到可提现金额为X
+        // 强行输入 X+1 会看到错误信息
+        // 调整为输入Y (Y<X)
+        // 此时验证手机号码
+        // 成功验证
+        // 会看到可提现金额为X-Y
+        // Px---以管理员身份
+        // 此时会看到该提现申请
+        // 点击拒绝
+        // -- 回到用户身份
+        // 会看到可提现金额回到X
+        //
+        // 重复流程2直置Px
+        // 此时会看到该提现申请
+        // 点击同意
+        // -- 回到用户身份
+        // 会看到可提现金额回到X-Y
+        // 同时看到已提现金额为Y
+    }
 
     @Test
     public void doWithdraw() throws Exception {
