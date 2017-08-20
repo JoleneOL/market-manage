bab27ad5776b7fd09bed16bc743ba1821a5e7fd9
Cai Jiang <luffy.ja@gmail.com>
2017-08-20 21:27:21 +0800

    #43 给客服的通知。

--- core-service/src/main/java/cn/lmjia/market/core/service/impl/NoticeServiceImpl.java
+++ core-service/src/main/java/cn/lmjia/market/core/service/impl/NoticeServiceImpl.java
@@ -3,7 +3,10 @@ package cn.lmjia.market.core.service.impl;
 import cn.lmjia.market.core.config.CoreConfig;
 import cn.lmjia.market.core.entity.MainOrder;
 import cn.lmjia.market.core.entity.request.PromotionRequest;
+import cn.lmjia.market.core.entity.support.ManageLevel;
+import cn.lmjia.market.core.service.LoginService;
 import cn.lmjia.market.core.service.MainOrderService;
+import cn.lmjia.market.core.service.ManagerService;
 import cn.lmjia.market.core.service.NoticeService;
 import cn.lmjia.market.core.service.SystemService;
 import cn.lmjia.market.core.service.request.PromotionRequestService;
@@ -18,12 +21,16 @@ import me.jiangcai.user.notice.wechat.WechatSendSupplier;
 import me.jiangcai.wx.model.WeixinUserDetail;
 import me.jiangcai.wx.model.message.SimpleTemplateMessageParameter;
 import me.jiangcai.wx.model.message.TemplateMessageParameter;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.math.BigDecimal;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
@@ -37,6 +44,7 @@ import java.util.Map;
 @Service
 public class NoticeServiceImpl implements NoticeService {
 
+    private static final Log log = LogFactory.getLog(NoticeServiceImpl.class);
     @Autowired
     private SystemService systemService;
     @Autowired
@@ -49,6 +57,10 @@ public class NoticeServiceImpl implements NoticeService {
     private MainOrderService mainOrderService;
     @Autowired
     private PromotionRequestService promotionRequestService;
+    @Autowired
+    private LoginService loginService;
+    @Autowired
+    private ManagerService managerService;
 
     @Override
     public boolean useLocal() {
@@ -97,6 +109,25 @@ public class NoticeServiceImpl implements NoticeService {
             }
 
         }, systemService.toUrl("/wechatOrderDetail?orderId={2}"));
+
+        wechatSendSupplier.registerTemplateMessage(new PaySuccessToCS(), new AbstractTemplateMessageStyle() {
+            @Override
+            public Collection<? extends TemplateMessageParameter> parameterStyles() {
+                return Arrays.asList(
+                        new SimpleTemplateMessageParameter("first", "{5}")
+                        , new SimpleTemplateMessageParameter("keyword1", "{2}")
+                        , new SimpleTemplateMessageParameter("keyword2", "{0,date,yyyy-MM-dd HH:mm}")
+                        , new SimpleTemplateMessageParameter("keyword3", "{3}")
+                        , new SimpleTemplateMessageParameter("keyword4", "{4}")
+                        , new SimpleTemplateMessageParameter("remark", "{6}")
+                );
+            }
+
+            @Override
+            public String getTemplateId() {
+                return useLocal() ? "V7Tu9FsG9L-WFgdrMPtcnWl3kv15_iKfz_yIoCbjtxY" : "Ibbpm1SUpkPdiVcNSffv75PlbQzjY2753q3951YL2RM";
+            }
+        }, null);
     }
 
     @Override
@@ -112,6 +143,20 @@ public class NoticeServiceImpl implements NoticeService {
                                 : new PaySuccessToJustOrder(), new Date(), order.getId(), order.getSerialId()
                         , order.getOrderProductName(), order.getOrderDueAmount());
             }
+
+            try {
+                userNoticeService.sendMessage(null, loginService.toWechatUser(managerService.levelAs(ManageLevel.customerService))
+                        , null, new PaySuccessToCS()
+                        , Date.from(ZonedDateTime.of(order.getOrderTime(), ZoneId.systemDefault()).toInstant())
+                        , order.getId(), order.getSerialId()
+                        , order.getOrderBody()
+                        , order.getInstallAddress().toString()
+                        , "客户已支付订单，请尽快发货"
+                        , ""
+                );
+            } catch (Throwable ex) {
+                log.trace("", ex);
+            }
         } else if (event.getPayableOrder() instanceof PromotionRequest) {
             PromotionRequest request = (PromotionRequest) event.getPayableOrder();
             WeixinUserDetail detail = request.getWhose().getWechatUser();
@@ -122,7 +167,22 @@ public class NoticeServiceImpl implements NoticeService {
                         , request.getOrderDueAmount()
                         , new Date());
             }
+
+            try {
+                userNoticeService.sendMessage(null, loginService.toWechatUser(managerService.levelAs(ManageLevel.customerService))
+                        , null, new PaySuccessToCS()
+                        , Date.from(ZonedDateTime.of(request.getRequestTime(), ZoneId.systemDefault()).toInstant())
+                        , request.getId(), String.valueOf(request.getId())
+                        , "合伙人升级服务"
+                        , "无"
+                        , "客户已支付订单，请检查相关资料，并完成合伙人升级"
+                        , ""
+                );
+            } catch (Throwable ex) {
+                log.trace("", ex);
+            }
         }
+
 
     }
 
@@ -142,6 +202,55 @@ public class NoticeServiceImpl implements NoticeService {
         };
     }
 
+    /**
+     * Date.class //时间 0 下单时间
+     * , Long.class //id 1
+     * , String.class//orderId 2
+     * , String.class//orderBody 3
+     * , String.class//address 4
+     * , String.class//first 5
+     * , String.class//first 6
+     */
+    private class PaySuccessToCS implements UserNoticeType {
+
+        @Override
+        public String id() {
+            return "PaySuccessToCS";
+        }
+
+        @Override
+        public String title() {
+            return "支付后的客服通知";
+        }
+
+        @Override
+        public boolean allowDifferentiation() {
+            return true;
+        }
+
+        @Override
+        public String defaultToText(Locale locale, Object[] parameters) {
+            return "客户已付款，请尽快配货";
+        }
+
+        @Override
+        public String defaultToHTML(Locale locale, Object[] parameters) {
+            return "客户已付款，请尽快配货";
+        }
+
+        @Override
+        public Class<?>[] expectedParameterTypes() {
+            return new Class<?>[]{
+                    Date.class //时间 0 下单时间
+                    , Long.class //id 1
+                    , String.class//orderId 2
+                    , String.class//orderBody
+                    , String.class//address
+                    , String.class//first
+                    , String.class//first
+            };
+        }
+    }
 
     /**
      * 区别是 并非订单收益者
