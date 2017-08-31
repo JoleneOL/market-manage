59bd5686b8b328a9d6dfc85a76f2e402495e1453
Cai Jiang <luffy.ja@gmail.com>
2017-08-31 20:57:17 +0800

    开始测试，居然忘了合并production先。

--- wechat-service/src/test/java/cn/lmjia/market/wechat/controller/my/WechatMyControllerTest.java
+++ wechat-service/src/test/java/cn/lmjia/market/wechat/controller/my/WechatMyControllerTest.java
@@ -2,9 +2,12 @@ package cn.lmjia.market.wechat.controller.my;
 
 import cn.lmjia.market.core.entity.Login;
 import cn.lmjia.market.wechat.WechatTestBase;
+import cn.lmjia.market.wechat.page.WechatMyPage;
 import org.junit.Test;
 
 /**
+ * 我的可以分为，我的团队，我的佣金
+ * 重点测试我的团队
  * @author CJ
  */
 public class WechatMyControllerTest extends WechatTestBase {
@@ -14,6 +17,15 @@ public class WechatMyControllerTest extends WechatTestBase {
     @Override
     protected Login allRunWith() {
         return login;
+    }
+
+    @Test
+    public void myTeam() {
+        // 假定我足够的高级 比如是一个顶级代理商 那么他可以看到下一季的
+        login = loginService.byLoginName("master");
+
+        WechatMyPage myPage = getWechatMyPage();
+
     }
 
     @Test
