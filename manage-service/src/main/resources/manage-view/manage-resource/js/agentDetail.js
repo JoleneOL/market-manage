2e8ad9f3f76da328fadcff6b57362b75ae6e333f
chang0022 <chang.0022@163.com>
2017-10-24 00:39:00 +0800

    代理商详情未完待续

--- manage-service/src/main/resources/manage-view/manage-resource/js/agentDetail.js
+++ manage-service/src/main/resources/manage-view/manage-resource/js/agentDetail.js
@@ -44,14 +44,137 @@ $(function () {
             });
         });
     });
-
+    var superiorInput = $("#superiorInput");
+    superiorInput.makeRecommendSelect()
     $('#J_modifySuperior').click(function () {
+        var self = $(this);
         layer.confirm('<strong class="text-danger">只有一次修改机会！</strong>', {
             icon: 3,
             title: '重要提示',
             btn: ['确定', '取消']
-        }, function () {
-            layer.msg('的确很重要', {icon: 1});
+        }, function (index) {
+            self.hide();
+            self.next().removeClass('hide');
+            layer.close(index);
+        });
+    });
+
+    $('#J_confirmModify').click(function () {
+        var $parent = $(this).parent();
+        var $form = $(this).closest('.form-horizontal');
+        var newSuperiorId = superiorInput.val();
+        var loading = layer.load();
+        $.ajax('/agent/superior/' + agentId, {
+            method: 'PUT',
+            data: {
+                newSuperior: newSuperiorId
+            },
+            dataType: 'json',
+            success: function (res) {
+                layer.close(loading);
+                if (res.resultCode !== 200) {
+                    layer.close(index);
+                    layer.msg('修改失败，稍后再试');
+                    return '';
+                }
+                $('#J_superiorName').text(res.data['name']);
+                $('#J_modifySuperior').remove();
+                $parent.remove();
+                $form.addClass('hide');
+                layer.msg('修改成功');
+            },
+            error: function () {
+                layer.close(loading);
+                layer.close(index);
+                layer.msg('修改失败，稍后再试');
+            }
+        });
+    });
+
+    $('#J_cancelModify').click(function () {
+        $(this).closest('.form-horizontal').addClass('hide');
+        $('#J_modifySuperior').show();
+    });
+    var $mobile = $('input[name="mobile"]');
+    var $authCode = $('input[name="authCode"]');
+    var sendAuthCodeUrl = $('body').attr('data-url-sendAuthCode');
+
+    $('#J_sendAuthCode').click(function () {
+        var self = $(this);
+        var mobile = $mobile.val();
+        if (!/^1([34578])\d{9}$/.test(mobile)) {
+            layer.msg('请输入正确的手机号');
+            return;
+        }
+        var loading = layer.load();
+        sendSMS(self);
+        $.ajax(sendAuthCodeUrl, {
+            method: 'POST',
+            data: {
+                mobile: mobile
+            },
+            dataType: 'json',
+            success: function (data) {
+                layer.close(loading);
+                if (data.resultCode !== 200) {
+                    layer.msg(data.resultMsg);
+                    return false;
+                }
+            },
+            error: function () {
+                layer.msg("系统错误");
+            }
+        })
+    });
+
+    function sendSMS(ele) {
+        ele.prop('disabled', true);
+        var s = 60;
+        var t = setInterval(function () {
+            ele.text(s-- + 's');
+            if (s === -1) {
+                clearInterval(t);
+                ele.text('获取验证码')
+                    .prop('disabled', false);
+            }
+        }, 1000);
+    }
+
+    $('#J_cancelModifyMobile').click(function () {
+        $mobile.val('');
+        $authCode.val('');
+    });
+    $('#J_confirmModifyMobile').click(function () {
+        if (!$mobile.val()) {
+            return layer.msg('手机号不能为空');
+        }
+        if (!$authCode.val()) {
+            return layer.msg('验证码不能为空');
+        }
+        var loading = layer.load();
+        $.ajax('/agent/mobile/' + agentId, {
+            method: 'PUT',
+            data: {
+                mobile: $mobile.val(),
+                $authCode: $authCode.val()
+            },
+            dataType: 'json',
+            success: function (res) {
+                layer.close(loading);
+                if (res.resultCode !== 200) {
+                    layer.msg(res.resultMsg);
+                    return '';
+                }
+                $('#J_mobile').text($mobile.val());
+                $('#J_mobileModal').modal('hide');
+                $mobile.val('');
+                $authCode.val('');
+                layer.msg('修改成功');
+            },
+            error: function () {
+                layer.close(loading);
+                layer.msg('修改失败，稍后再试');
+            }
         });
     });
 });
--- manage-service/src/main/resources/manage-view/mock/agent.js
+++ manage-service/src/main/resources/manage-view/mock/agent.js
@@ -6,3 +6,23 @@ Mock.mock(/\/agent\/name\/\d/, {
     "resultCode": 200,
     "resultMsg": "ok"
 });
+
+Mock.mock(/\/agent\/superior\/\d/, {
+    "resultCode": 200,
+    "resultMsg": "ok",
+    "data": {
+        name: '@cname'
+    }
+});
+
+Mock.mock(/\/api\/authCode/, "post", {
+    "resultCode": 200,
+    "resultMsg": "该手机号已经被注册",
+    "data": null
+});
+
+
+Mock.mock(/\/agent\/mobile\/\d/, {
+    "resultCode": 200,
+    "resultMsg": "ok"
+});
