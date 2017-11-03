92e98704af9676ec2dc87c6e0cbc2b74567a48a9
Ellusion <lixuefeng2012@126.com>
2017-11-03 19:56:13 +0800

    js真给我搞疯了

--- wechat-service/src/main/resources/wechat-resource/assets/js/helpCenter/helpCenter.js
+++ wechat-service/src/main/resources/wechat-resource/assets/js/helpCenter/helpCenter.js
@@ -53,8 +53,8 @@ $(function () {
         if (data.length > 0) {
             str = '<div class="weui-cells__title">相关问题</div><div class="weui-cells">';
             $.each(data, function (i, v) {
-                str += '<a class="weui-cell weui-cell_access" href="helpDetail.html?id=' + v.helpId + '" th:href="@{/commonProblemDetail/}'+v.id+'" >' +
-                    '  <div class="weui-cell__bd" th:text="'+v.title+'">' + v.helpTitle + '</div>' +
+                str += '<a class="weui-cell weui-cell_access" href="helpDetail.html?id=' + v.helpId + '" th:href="@{/commonProblemDetail/'+v.id+'}" >' +
+                    '  <div class="weui-cell__bd" th:utext="'+v.title+'">' + v.helpTitle + '</div>' +
                     '  <div class="weui-cell__ft"></div>' +
                     '</a>'
             });
